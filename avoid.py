8bb795ad36f37168db23f1707dfad16f755569e1
Neil Bizzell <NBizzell@users.noreply.github.com>
2015-03-10 22:15:07 +0000

    Update avoid.py

--- avoid.py
+++ avoid.py
@@ -1,1 +1,40 @@
+import agobo
+import time
+import mouth
 
+agobo.init
+
+speed = 50
+angle = 1.08
+space = 10
+
+try:
+  while True:
+    mouth.smile()
+    agobo.forward(speed)
+    dist = agobo.getDistance()
+    print "distance: ", int(dist)
+    if dist < space:
+      mouth.sad()
+      agobo.spinRight(speed)
+      time.sleep(angle)
+      agobo.stop()
+      dist = agobo.getDistance()
+      if dist < (space*2):
+        mouth.oh()
+        agobo.spinLeft(speed)
+        time.sleep(angle*2)
+        agobo.stop()
+        dist = agobo.getDistance()
+        if dist < (space*2):
+          mouth.conf1()
+          agobo.spinLeft(speed)
+          time.sleep(angle)
+  time.sleep(0.25)
+  mouth.smile()
+except KeyboardIterupt:
+  print
+  
+finally:
+  agobo.cleanup()
+  
